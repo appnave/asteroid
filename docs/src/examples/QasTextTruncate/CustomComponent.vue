b5a59a850005b846be35e8ce819d8915f11bdd17
felipejuzo02 <felipejuzo@hotmail.com>
2025-12-15 10:56:28 -0300

    feat: custom truncate description

---
+++ docs/src/examples/QasTextTruncate/CustomComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <qas-alert class="q-mb-md" text="Texto de teste" />
+
+    <div>Este é um componente customizado para a descrição do dialog.</div>
+  </div>
+</template>
+
+<script setup>
+defineOptions({ name: 'CustomComponent' })
+</script>
