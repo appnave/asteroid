368d6b9ecc1b0ca43da845776046fd4595cafa6d
Elias <elias3748@gmail.com>
2026-07-21 16:29:45 -0300

    Releasing v3.20.0-beta.27-alpha.1

--- app-extension/src/index.js
+++ app-extension/src/index.js
@@ -102,7 +102,10 @@ export default async function (api) {
     'asteroid-config-app': asteroidConfigPath,
     'vue-router': api.resolve.app(vueRouter),
     asteroid: api.resolve.app(asteroid),
-    quasar: api.resolve.app(quasar)
+    quasar: api.resolve.app(quasar),
+    'images/layers.png': api.resolve.app('node_modules/leaflet/dist/images/layers.png'),
+    'images/layers-2x.png': api.resolve.app('node_modules/leaflet/dist/images/layers-2x.png'),
+    'images/marker-icon.png': api.resolve.app('node_modules/leaflet/dist/images/marker-icon.png')
   }
 
   if (api.hasVite) {
@@ -114,13 +117,13 @@ export default async function (api) {
        * utiliza imagens que são referenciadas diretamente em seu código, e não é possível alterar isso.
        * Dessa forma, é necessário configurar os aliases para que o vite consiga resolver os paths corretamente.
        */
-      const leafletImageAliases = {
-        'images/layers.png': api.resolve.app('node_modules/leaflet/dist/images/layers.png'),
-        'images/layers-2x.png': api.resolve.app('node_modules/leaflet/dist/images/layers-2x.png'),
-        'images/marker-icon.png': api.resolve.app('node_modules/leaflet/dist/images/marker-icon.png')
-      }
+      // const leafletImageAliases = {
+      //   'images/layers.png': api.resolve.app('node_modules/leaflet/dist/images/layers.png'),
+      //   'images/layers-2x.png': api.resolve.app('node_modules/leaflet/dist/images/layers-2x.png'),
+      //   'images/marker-icon.png': api.resolve.app('node_modules/leaflet/dist/images/marker-icon.png')
+      // }
 
-      viteConf.resolve.alias = Object.assign(leafletImageAliases, viteConf.resolve.alias, alias)
+      Object.assign(viteConf.resolve.alias, alias)
 
       // optimizeDeps (necessário para funcionamento do QasMap)
       viteConf.optimizeDeps = viteConf.optimizeDeps || {}
@@ -154,12 +157,12 @@ export default async function (api) {
    * utiliza imagens que são referenciadas diretamente em seu código, e não é possível alterar isso.
    * Dessa forma, é necessário configurar os aliases para que o webpack consiga resolver os paths corretamente.
    */
-  api.chainWebpack(chain => {
-    chain.resolve.alias
-      .set('images/layers.png', api.resolve.app('node_modules/leaflet/dist/images/layers.png'))
-      .set('images/layers-2x.png', api.resolve.app('node_modules/leaflet/dist/images/layers-2x.png'))
-      .set('images/marker-icon.png', api.resolve.app('node_modules/leaflet/dist/images/marker-icon.png'))
-  })
+  // api.chainWebpack(chain => {
+  //   chain.resolve.alias
+  //     .set('images/layers.png', api.resolve.app('node_modules/leaflet/dist/images/layers.png'))
+  //     .set('images/layers-2x.png', api.resolve.app('node_modules/leaflet/dist/images/layers-2x.png'))
+  //     .set('images/marker-icon.png', api.resolve.app('node_modules/leaflet/dist/images/marker-icon.png'))
+  // })
 
   api.extendWebpack(webpack => {
     Object.assign(webpack.resolve.alias, alias)
